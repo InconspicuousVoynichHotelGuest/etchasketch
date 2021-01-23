0d5dab2747bdc4685c57d5517d7e3da25fa9f74d
Voynich Hotel <sieglein201@hotmail.de>
2021-01-23 20:11:15 +0100

    Final project, I can't into css so basic layout it is

--- etch.js
+++ etch.js
@@ -1,16 +1,49 @@
     
     const divcontainer = document.querySelector(".container");
-    
-    for(let i = 0; i < (16*16); i++) {
+    const buttonselector = document.querySelector("button");
+    const root = document.documentElement;
+    let userinput = 16;
+    var reg = /^\d+$/;
+
+
+    function makeGrid(userdata) {
+    for(let i = 0; i < (userdata * userdata); i++) {
     const gridElement = document.createElement("div");
     gridElement.classList.add('flexelement');
+    gridElement.addEventListener('mouseenter', (e) => {
+        e.target.classList.add('colourchanger');
+      });
     divcontainer.appendChild(gridElement);
+        }
+    };
+//function addcolour() {
+ //   flexielements.forEach((element) => {
+ //   element.addEventListener('mouseenter', (e) => {
+      //  e.target.style.backgroundColor = "blue";
+ //     e.target.classList.add('colourchanger');
+ //   });
+//});
+//};
+   makeGrid(userinput);
+  // addcolour();
+  //  flexielements.forEach((element) => {
+   //     element.addEventListener('mouseenter', (e) => {
+   //       e.target.classList.add('colourchanger');
+   //     });
+   // });
 
-}
-    const flexielements = document.querySelectorAll(".flexelement");
-    flexielements.forEach((element) => {
-        element.addEventListener('mouseenter', (e) => {
-          //  e.target.style.backgroundColor = "blue";
-          e.target.classList.add('colourchanger');
-        });
+    buttonselector.addEventListener('click', function() {
+        while(divcontainer.firstChild) {
+            divcontainer.removeChild(divcontainer.firstChild);
+        }
+       // flexielements.forEach((element) => {
+       //     element.classList.remove('colourchanger');
+       // });
+        userinput = prompt("Select the size of the grid:");
+        if(userinput > 100|| reg.test(userinput) !== true) {
+            userinput = prompt("only numbers between 1 and 100");
+        };
+        root.style.setProperty("--gridlength", userinput);
+        makeGrid(userinput);
     });
+    
